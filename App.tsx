361a3a37084ffef97dcc46369e2e26304c883e17
gokhanyanik <gokhanyanik@gmail.com>
2024-11-02 19:00:05 +0300

    Hesap makinesi Tasarım Ve Fonksiyonlar Eklendi

--- App.tsx
+++ App.tsx
@@ -19,101 +19,137 @@ function App(): React.JSX.Element {
 
   const [total, setTotal] = useState("")
 
+  const handleButtonPress = (value: string) => {
+
+    if (value === "Ac") {
+      setTotal("")
+    } else if (value === "back") {
+      setTotal(total.slice(0, -1))
+
+    } else if (value === "=") {
+      try {
+        const result = eval(total)
+        setTotal(String(result))
+      } catch (error) {
+        setTotal("error")
+      //  setTotal("")
+      }
+    } else if (value === "+") {
+      setTotal(total + "+")
+    } else if (value === "-") {
+      setTotal(total + "-")
+    } else if (value === "*") {
+      setTotal(total + "*")
+    } else if (value === "/") {
+      setTotal(total + "/")
+    } else {
+      setTotal(total+value)
+    }
+  }
+
   return (
     <LinearGradient
-      colors={['white', '#ADD8FF']}
-      start={{ x: -0.1, y: 0 }}
-      end={{ x: 1.1, y: 0 }}
+      colors={['#F7F8FB', '#B0E2FF']}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
       style={styles.gradient}
     >
 
 
 
       <View style={{ flex: 1, alignItems: "stretch", left: 15 }}>
+        <View style={{ height: 1, backgroundColor: "red" }}>
 
+          <TextInput
+            style={styles.input}
+            placeholder='0'
+            value={total}
+            onChangeText={setTotal}
+          />
+        </View>
         <View style={{ flexDirection: "row" }}>
-          <TouchableOpacity style={styles.tuse} onPress={() => console.log("merhaba")} >
+          <TouchableOpacity style={styles.tuse} onPress={() => handleButtonPress("e")} >
             <Text style={styles.buttontext2}>e</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.tusµ} onPress={() => console.log("merhaba")}>
+          <TouchableOpacity style={styles.tusµ} onPress={() => handleButtonPress("3.14")}>
             <Text style={styles.buttontext2}>µ</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.tussin} onPress={() => console.log("merhaba")} >
+          <TouchableOpacity style={styles.tussin} onPress={() => handleButtonPress("sin")} >
             <Text style={styles.buttontext2}>sin</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.tusdeg} onPress={() => console.log("merhaba")}>
+          <TouchableOpacity style={styles.tusdeg} onPress={() => handleButtonPress("deg")}>
             <Text style={styles.buttontext2}>deg</Text>
           </TouchableOpacity>
         </View>
 
         <View style={{ flexDirection: "row" }}>
-          <TouchableOpacity style={styles.tusac} onPress={() => console.log("merhaba")} >
-            <Text style={[styles.buttontext, { color: "blue" }]}>Ac</Text>
+          <TouchableOpacity style={styles.tusac} onPress={() => handleButtonPress("Ac")} >
+            <Text style={[styles.buttontext, { color: "#C9DFF1" }]}>Ac</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.tusbackspace} onPress={() => console.log("merhaba")}>
+          <TouchableOpacity style={styles.tusbackspace} onPress={() => handleButtonPress("back")}>
             <Image
-            source={require('./assets/images/icon.png')}
-            style={styles.icon}
+              source={require('./assets/images/icon.png')}
+              style={styles.icon}
             />
           </TouchableOpacity>
-          <TouchableOpacity style={styles.tusbölme} onPress={() => console.log("merhaba")}>
+          <TouchableOpacity style={styles.tusbölme} onPress={() => handleButtonPress("/")}>
             <Text style={[styles.buttontext, { color: "blue" }]}>/</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.tuscarpma} onPress={() => console.log("merhaba")}>
+          <TouchableOpacity style={styles.tuscarpma} onPress={() => handleButtonPress("*")}>
             <Text style={[styles.buttontext, { color: "blue" }]}>*</Text>
           </TouchableOpacity>
         </View>
 
         <View style={{ flexDirection: "row" }}>
-          <TouchableOpacity style={styles.tus7} onPress={() => console.log("merhaba")}>
-            <Text style={styles.buttontext}>7</Text>
+          <TouchableOpacity style={styles.tus7} onPress={() => handleButtonPress("7")}>
+            <Text style={[styles.buttontext, { fontSize: 25 }]}>7</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.tus8} onPress={() => console.log("merhaba")} >
-            <Text style={styles.buttontext}>8</Text>
+          <TouchableOpacity style={styles.tus8} onPress={() => handleButtonPress("8")} >
+            <Text style={[styles.buttontext, { fontSize: 25 }]}>8</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.tus9} onPress={() => console.log("merhaba")}>
-            <Text style={styles.buttontext}>9</Text>
+          <TouchableOpacity style={styles.tus9} onPress={() => handleButtonPress("9")}>
+            <Text style={[styles.buttontext, { fontSize: 25 }]}>9</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.tuscikarma} onPress={() => console.log("merhaba")}>
+          <TouchableOpacity style={styles.tuscikarma} onPress={() => handleButtonPress("-")}>
             <Text style={[styles.buttontext, { color: "blue" }]}>-</Text>
           </TouchableOpacity>
         </View>
 
         <View style={{ flexDirection: "row" }} >
-          <TouchableOpacity style={styles.tus4} onPress={() => console.log("merhaba")}>
-            <Text style={styles.buttontext}>4</Text>
+          <TouchableOpacity style={styles.tus4} onPress={() => handleButtonPress("4")}>
+            <Text style={[styles.buttontext, { fontSize: 25 }]}>4</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.tus5} onPress={() => console.log("merhaba")} >
-            <Text style={styles.buttontext}>5</Text>
+          <TouchableOpacity style={styles.tus5} onPress={() => handleButtonPress("5")} >
+            <Text style={[styles.buttontext, { fontSize: 25 }]}>5</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.tus6} onPress={() => console.log("merhaba")}>
-            <Text style={styles.buttontext}>6</Text>
+          <TouchableOpacity style={styles.tus6} onPress={() => handleButtonPress("6")}>
+            <Text style={[styles.buttontext, { fontSize: 25 }]}>6</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.tustopla} onPress={() => console.log("merhaba")}>
+          <TouchableOpacity style={styles.tustopla} onPress={() => handleButtonPress("+")}>
             <Text style={[styles.buttontext, { color: "blue" }]}>+</Text>
           </TouchableOpacity>
         </View>
 
         <View style={{ flexDirection: "row" }}>
-          <TouchableOpacity style={styles.tus1} onPress={() => console.log("merhaba")}>
-            <Text style={styles.buttontext}>1</Text>
+          <TouchableOpacity style={styles.tus1} onPress={() => handleButtonPress("1")}>
+            <Text style={[styles.buttontext, { fontSize: 25 }]}>1</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.tus2} onPress={() => console.log("merhaba")}>
-            <Text style={styles.buttontext}>2</Text>
+          <TouchableOpacity style={styles.tus2} onPress={() => handleButtonPress("2")}>
+            <Text style={[styles.buttontext, { fontSize: 25 }]}>2</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.tus3} onPress={() => console.log("merhaba")}>
-            <Text style={styles.buttontext}>3</Text>
+          <TouchableOpacity style={styles.tus3} onPress={() => handleButtonPress("3")}>
+            <Text style={[styles.buttontext, { fontSize: 25 }]}>3</Text>
           </TouchableOpacity>
         </View>
 
         <View style={{ flexDirection: "row" }}>
-          <TouchableOpacity style={styles.tus0} onPress={() => console.log("merhaba")}>
-            <Text style={styles.buttontext}>0</Text>
+          <TouchableOpacity style={styles.tus0} onPress={() => handleButtonPress("0")}>
+            <Text style={[styles.buttontext, { fontSize: 25 }]}>0</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.tusnokta} onPress={() => console.log("merhaba")}>
-            <Text style={styles.buttontext}>.</Text>
+          <TouchableOpacity style={styles.tusnokta} onPress={() => handleButtonPress(".")}>
+            <Text style={[styles.buttontext, { fontSize: 25 }]}>.</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.tusesittir} onPress={() => console.log("merhaba")}>
+          <TouchableOpacity style={styles.tusesittir} onPress={() => handleButtonPress("=")}>
             <Text style={[styles.buttontext, { color: "white" }]}>=</Text>
           </TouchableOpacity>
 
@@ -126,19 +162,31 @@ function App(): React.JSX.Element {
   );
 }
 const styles = StyleSheet.create({
+  input: {
+    height: 180,
+    borderColor: 'gray',
+    borderWidth: 0,
+    marginBottom: 12,
+    padding: 20,
+    width: '100%',
+    top: 90,
+    textAlign: "right",
+    fontSize:30
+  },
 
-  icon:{
-    width:21.8,
-    height:18,
-    color:"red"
+  icon: {
+    width: 21.8,
+    height: 18,
+    color: "#C9DFF1"
   },
   gradient: {
     flex: 1,
     //justifyContent:"space-between"
+
   },
   buttontext: {
     width: 25,
-    height: 48,
+    height: 52,
     fontFamily: 'Poppins', // Poppins fontunu kullanmak için eklenmiş olmalı
     fontSize: 20,
     fontWeight: '900',
@@ -146,23 +194,23 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: "white",
     // background-blend-mode: overlay,
-    textShadowRadius: 10
+    // textShadowRadius: 10
 
   },
   buttontext2: {
-    width: 30,
+    width: 35,
     height: 48,
     fontFamily: 'Poppins', // Poppins fontunu kullanmak için eklenmiş olmalı
-    fontSize: 15,
+    fontSize: 19,
     fontWeight: '900',
     lineHeight: 45,
-    textAlign: 'left',
+    textAlign: 'center',
     color: "white",
-    textShadowRadius: 10
+    // textShadowRadius: 10
   },
   tuse: {
 
-    backgroundColor: "#E6F8FF",
+    backgroundColor: "#e3ecf6",
     justifyContent: "center",
     alignItems: "center",
     width: 62,
@@ -179,7 +227,7 @@ const styles = StyleSheet.create({
     margin: 5
   },
   tusµ: {
-    backgroundColor: "#E6F8FF",
+    backgroundColor: "#e3ecf6",
     justifyContent: "center",
     alignItems: "center",
     width: 62,
@@ -196,7 +244,7 @@ const styles = StyleSheet.create({
     margin: 5
   },
   tussin: {
-    backgroundColor: "#E6F8FF",
+    backgroundColor: "#e3ecf6",
     justifyContent: "center",
     alignItems: "center",
     width: 62,
@@ -213,7 +261,7 @@ const styles = StyleSheet.create({
     margin: 5
   },
   tusdeg: {
-    backgroundColor: "#E6F8FF",
+    backgroundColor: "#e3ecf6",
     justifyContent: "center",
     alignItems: "center",
     width: 62,
@@ -230,7 +278,7 @@ const styles = StyleSheet.create({
     margin: 5
   },
   tusac: {
-    backgroundColor: "#E6F8FF",
+    backgroundColor: "#e3ecf6",
     justifyContent: "center",
     alignItems: "center",
     width: 62,
@@ -247,7 +295,7 @@ const styles = StyleSheet.create({
     margin: 5
   },
   tusbackspace: {
-    backgroundColor: "#E6F8FF",
+    backgroundColor: "#e3ecf6",
     justifyContent: "center",
     alignItems: "center",
     width: 62,
@@ -298,7 +346,7 @@ const styles = StyleSheet.create({
     margin: 5
   },
   tus7: {
-    backgroundColor: "#E6F8FF",
+    backgroundColor: "#e3ecf6",
     justifyContent: "center",
     alignItems: "center",
     width: 62,
@@ -315,7 +363,7 @@ const styles = StyleSheet.create({
     margin: 5
   },
   tus8: {
-    backgroundColor: "#E6F8FF",
+    backgroundColor: "#e3ecf6",
     justifyContent: "center",
     alignItems: "center",
     width: 62,
@@ -332,7 +380,7 @@ const styles = StyleSheet.create({
     margin: 5
   },
   tus9: {
-    backgroundColor: "#E6F8FF",
+    backgroundColor: "#e3ecf6",
     justifyContent: "center",
     alignItems: "center",
     width: 62,
@@ -366,7 +414,7 @@ const styles = StyleSheet.create({
     margin: 5
   },
   tus4: {
-    backgroundColor: "#E6F8FF",
+    backgroundColor: "#e3ecf6",
     justifyContent: "center",
     alignItems: "center",
     width: 62,
@@ -383,7 +431,7 @@ const styles = StyleSheet.create({
     margin: 5
   },
   tus5: {
-    backgroundColor: "#E6F8FF",
+    backgroundColor: "#e3ecf6",
     justifyContent: "center",
     alignItems: "center",
     width: 62,
@@ -400,7 +448,7 @@ const styles = StyleSheet.create({
     margin: 5
   },
   tus6: {
-    backgroundColor: "#E6F8FF",
+    backgroundColor: "#e3ecf6",
     justifyContent: "center",
     alignItems: "center",
     width: 62,
@@ -434,7 +482,7 @@ const styles = StyleSheet.create({
     margin: 5
   },
   tus1: {
-    backgroundColor: "#E6F8FF",
+    backgroundColor: "#e3ecf6",
     justifyContent: "center",
     alignItems: "center",
     width: 62,
@@ -451,7 +499,7 @@ const styles = StyleSheet.create({
     margin: 5
   },
   tus2: {
-    backgroundColor: "#E6F8FF",
+    backgroundColor: "#e3ecf6",
     justifyContent: "center",
     alignItems: "center",
     width: 62,
@@ -468,7 +516,7 @@ const styles = StyleSheet.create({
     margin: 5
   },
   tus3: {
-    backgroundColor: "#E6F8FF",
+    backgroundColor: "#e3ecf6",
     justifyContent: "center",
     alignItems: "center",
     width: 62,
@@ -485,7 +533,7 @@ const styles = StyleSheet.create({
     margin: 5
   },
   tus0: {
-    backgroundColor: "#E6F8FF",
+    backgroundColor: "#e3ecf6",
     justifyContent: "center",
     alignItems: "center",
     width: 142,
@@ -502,7 +550,7 @@ const styles = StyleSheet.create({
     margin: 5
   },
   tusnokta: {
-    backgroundColor: "#E6F8FF",
+    backgroundColor: "#e3ecf6",
     justifyContent: "center",
     alignItems: "center",
     width: 62,
