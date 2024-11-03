e7a8fc452a499904eec7f72c0ddb9775618ac4e6
gokhanyanik <gokhanyanik@gmail.com>
2024-11-03 15:49:59 +0300

    Satır Boşlukları Kaldırıldı
    Genel kod düzeni için boşluklar kaldırıldı ve düzenlemeler yapıldı.

--- App.tsx
+++ App.tsx
@@ -1,8 +1,5 @@
 import React, { useState } from 'react';
 import LinearGradient from 'react-native-linear-gradient';
-import Icon from 'react-native-vector-icons/MaterialIcons';
-
-
 import {
   StyleSheet,
   Text,
@@ -13,26 +10,21 @@ import {
 } from 'react-native';
 
 
-
-
 function App(): React.JSX.Element {
 
   const [total, setTotal] = useState("")
 
   const handleButtonPress = (value: string) => {
-
     if (value === "Ac") {
       setTotal("")
     } else if (value === "back") {
       setTotal(total.slice(0, -1))
-
     } else if (value === "=") {
       try {
         const result = eval(total)
         setTotal(String(result))
       } catch (error) {
         setTotal("error")
-      //  setTotal("")
       }
     } else if (value === "+") {
       setTotal(total + "+")
@@ -43,7 +35,7 @@ function App(): React.JSX.Element {
     } else if (value === "/") {
       setTotal(total + "/")
     } else {
-      setTotal(total+value)
+      setTotal(total + value)
     }
   }
 
@@ -54,12 +46,8 @@ function App(): React.JSX.Element {
       end={{ x: 1, y: 1 }}
       style={styles.gradient}
     >
-
-
-
       <View style={{ flex: 1, alignItems: "stretch", left: 15 }}>
-        <View style={{ height: 1, backgroundColor: "red" }}>
-
+        <View style={{ height: 1 }}>
           <TextInput
             style={styles.input}
             placeholder='0'
@@ -81,7 +69,6 @@ function App(): React.JSX.Element {
             <Text style={styles.buttontext2}>deg</Text>
           </TouchableOpacity>
         </View>
-
         <View style={{ flexDirection: "row" }}>
           <TouchableOpacity style={styles.tusac} onPress={() => handleButtonPress("Ac")} >
             <Text style={[styles.buttontext, { color: "#C9DFF1" }]}>Ac</Text>
@@ -99,7 +86,6 @@ function App(): React.JSX.Element {
             <Text style={[styles.buttontext, { color: "blue" }]}>*</Text>
           </TouchableOpacity>
         </View>
-
         <View style={{ flexDirection: "row" }}>
           <TouchableOpacity style={styles.tus7} onPress={() => handleButtonPress("7")}>
             <Text style={[styles.buttontext, { fontSize: 25 }]}>7</Text>
@@ -114,7 +100,6 @@ function App(): React.JSX.Element {
             <Text style={[styles.buttontext, { color: "blue" }]}>-</Text>
           </TouchableOpacity>
         </View>
-
         <View style={{ flexDirection: "row" }} >
           <TouchableOpacity style={styles.tus4} onPress={() => handleButtonPress("4")}>
             <Text style={[styles.buttontext, { fontSize: 25 }]}>4</Text>
@@ -129,7 +114,6 @@ function App(): React.JSX.Element {
             <Text style={[styles.buttontext, { color: "blue" }]}>+</Text>
           </TouchableOpacity>
         </View>
-
         <View style={{ flexDirection: "row" }}>
           <TouchableOpacity style={styles.tus1} onPress={() => handleButtonPress("1")}>
             <Text style={[styles.buttontext, { fontSize: 25 }]}>1</Text>
@@ -141,7 +125,6 @@ function App(): React.JSX.Element {
             <Text style={[styles.buttontext, { fontSize: 25 }]}>3</Text>
           </TouchableOpacity>
         </View>
-
         <View style={{ flexDirection: "row" }}>
           <TouchableOpacity style={styles.tus0} onPress={() => handleButtonPress("0")}>
             <Text style={[styles.buttontext, { fontSize: 25 }]}>0</Text>
@@ -154,10 +137,7 @@ function App(): React.JSX.Element {
           </TouchableOpacity>
 
         </View>
-
       </View>
-
-
     </LinearGradient>
   );
 }
@@ -171,18 +151,15 @@ const styles = StyleSheet.create({
     width: '100%',
     top: 90,
     textAlign: "right",
-    fontSize:30
+    fontSize: 30
   },
-
   icon: {
     width: 21.8,
     height: 18,
     color: "#C9DFF1"
   },
   gradient: {
-    flex: 1,
-    //justifyContent:"space-between"
-
+    flex: 1
   },
   buttontext: {
     width: 25,
@@ -193,9 +170,6 @@ const styles = StyleSheet.create({
     lineHeight: 45,
     textAlign: 'center',
     color: "white",
-    // background-blend-mode: overlay,
-    // textShadowRadius: 10
-
   },
   buttontext2: {
     width: 35,
@@ -206,10 +180,8 @@ const styles = StyleSheet.create({
     lineHeight: 45,
     textAlign: 'center',
     color: "white",
-    // textShadowRadius: 10
   },
   tuse: {
-
     backgroundColor: "#e3ecf6",
     justifyContent: "center",
     alignItems: "center",
@@ -218,7 +190,6 @@ const styles = StyleSheet.create({
     top: 320,
     left: 34,
     gap: 10,
-    //borderTopWidth: 1,   // Üst kenara 1 px kalınlığında sınır
     borderRadius: 16,
     paddingTop: 6,   // üst kenar boşluğu,içeri doğru
     paddingBottom: 6,// alt kenar boşluğu,içeri doğru
@@ -235,7 +206,6 @@ const styles = StyleSheet.create({
     top: 320,
     left: 40,
     gap: 10,
-    //  borderTopWidth: 1,   // Üst kenara 1 px kalınlığında sınır
     borderRadius: 16,
     paddingTop: 6,   // üst kenar boşluğu,içeri doğru
     paddingBottom: 6,// alt kenar boşluğu,içeri doğru
@@ -252,7 +222,6 @@ const styles = StyleSheet.create({
     top: 320,
     left: 46,
     gap: 10,
-    //  borderTopWidth: 1,   // Üst kenara 1 px kalınlığında sınır
     borderRadius: 16,
     paddingTop: 6,   // üst kenar boşluğu,içeri doğru
     paddingBottom: 6,// alt kenar boşluğu,içeri doğru
@@ -269,7 +238,6 @@ const styles = StyleSheet.create({
     top: 320,
     left: 52,
     gap: 10,
-    // borderTopWidth: 1,   // Üst kenara 1 px kalınlığında sınır
     borderRadius: 16,
     paddingTop: 6,   // üst kenar boşluğu,içeri doğru
     paddingBottom: 6,// alt kenar boşluğu,içeri doğru
@@ -286,7 +254,6 @@ const styles = StyleSheet.create({
     top: 326,
     left: 34,
     gap: 10,
-    //borderTopWidth: 1,   // Üst kenara 1 px kalınlığında sınır
     borderRadius: 16,
     paddingTop: 6,   // üst kenar boşluğu,içeri doğru
     paddingBottom: 6,// alt kenar boşluğu,içeri doğru
@@ -303,7 +270,6 @@ const styles = StyleSheet.create({
     top: 326,
     left: 40,
     gap: 10,
-    // borderTopWidth: 1,   // Üst kenara 1 px kalınlığında sınır
     borderRadius: 16,
     paddingTop: 6,   // üst kenar boşluğu,içeri doğru
     paddingBottom: 6,// alt kenar boşluğu,içeri doğru
@@ -320,7 +286,6 @@ const styles = StyleSheet.create({
     top: 326,
     left: 46,
     gap: 10,
-    //borderTopWidth: 1,   // Üst kenara 1 px kalınlığında sınır
     borderRadius: 16,
     paddingTop: 6,   // üst kenar boşluğu,içeri doğru
     paddingBottom: 6,// alt kenar boşluğu,içeri doğru
@@ -337,7 +302,6 @@ const styles = StyleSheet.create({
     top: 326,
     left: 52,
     gap: 10,
-    // borderTopWidth: 1,   // Üst kenara 1 px kalınlığında sınır
     borderRadius: 16,
     paddingTop: 6,   // üst kenar boşluğu,içeri doğru
     paddingBottom: 6,// alt kenar boşluğu,içeri doğru
@@ -354,7 +318,6 @@ const styles = StyleSheet.create({
     top: 338,
     left: 34,
     gap: 10,
-    //  borderTopWidth: 1,   // Üst kenara 1 px kalınlığında sınır
     borderRadius: 16,
     paddingTop: 6,   // üst kenar boşluğu,içeri doğru
     paddingBottom: 6,// alt kenar boşluğu,içeri doğru
@@ -371,7 +334,6 @@ const styles = StyleSheet.create({
     top: 338,
     left: 40,
     gap: 10,
-    //  borderTopWidth: 1,   // Üst kenara 1 px kalınlığında sınır
     borderRadius: 16,
     paddingTop: 6,   // üst kenar boşluğu,içeri doğru
     paddingBottom: 6,// alt kenar boşluğu,içeri doğru
@@ -388,7 +350,6 @@ const styles = StyleSheet.create({
     top: 338,
     left: 44,
     gap: 10,
-    //   borderTopWidth: 1,   // Üst kenara 1 px kalınlığında sınır
     borderRadius: 16,
     paddingTop: 6,   // üst kenar boşluğu,içeri doğru
     paddingBottom: 6,// alt kenar boşluğu,içeri doğru
@@ -405,7 +366,6 @@ const styles = StyleSheet.create({
     top: 338,
     left: 50,
     gap: 10,
-    // borderTopWidth: 1,   // Üst kenara 1 px kalınlığında sınır
     borderRadius: 16,
     paddingTop: 6,   // üst kenar boşluğu,içeri doğru
     paddingBottom: 6,// alt kenar boşluğu,içeri doğru
@@ -422,7 +382,6 @@ const styles = StyleSheet.create({
     top: 344,
     left: 34,
     gap: 10,
-    //    borderTopWidth: 1,   // Üst kenara 1 px kalınlığında sınır
     borderRadius: 16,
     paddingTop: 6,   // üst kenar boşluğu,içeri doğru
     paddingBottom: 6,// alt kenar boşluğu,içeri doğru
@@ -439,7 +398,6 @@ const styles = StyleSheet.create({
     top: 344,
     left: 40,
     gap: 10,
-    // borderTopWidth: 1,   // Üst kenara 1 px kalınlığında sınır
     borderRadius: 16,
     paddingTop: 6,   // üst kenar boşluğu,içeri doğru
     paddingBottom: 6,// alt kenar boşluğu,içeri doğru
@@ -456,7 +414,6 @@ const styles = StyleSheet.create({
     top: 344,
     left: 46,
     gap: 10,
-    //   borderTopWidth: 1,   // Üst kenara 1 px kalınlığında sınır
     borderRadius: 16,
     paddingTop: 6,   // üst kenar boşluğu,içeri doğru
     paddingBottom: 6,// alt kenar boşluğu,içeri doğru
@@ -473,7 +430,6 @@ const styles = StyleSheet.create({
     top: 345,
     left: 52,
     gap: 10,
-    // borderTopWidth: 1,   // Üst kenara 1 px kalınlığında sınır
     borderRadius: 16,
     paddingTop: 6,   // üst kenar boşluğu,içeri doğru
     paddingBottom: 6,// alt kenar boşluğu,içeri doğru
@@ -490,7 +446,6 @@ const styles = StyleSheet.create({
     top: 315,
     left: 34,
     gap: 10,
-    // borderTopWidth: 1,   // Üst kenara 1 px kalınlığında sınır
     borderRadius: 16,
     paddingTop: 6,   // üst kenar boşluğu,içeri doğru
     paddingBottom: 6,// alt kenar boşluğu,içeri doğru
@@ -507,7 +462,6 @@ const styles = StyleSheet.create({
     top: 315,
     left: 40,
     gap: 10,
-    // borderTopWidth: 1,   // Üst kenara 1 px kalınlığında sınır
     borderRadius: 16,
     paddingTop: 6,   // üst kenar boşluğu,içeri doğru
     paddingBottom: 6,// alt kenar boşluğu,içeri doğru
@@ -524,7 +478,6 @@ const styles = StyleSheet.create({
     top: 315,
     left: 46,
     gap: 10,
-    // borderTopWidth: 1,   // Üst kenara 1 px kalınlığında sınır
     borderRadius: 16,
     paddingTop: 6,   // üst kenar boşluğu,içeri doğru
     paddingBottom: 6,// alt kenar boşluğu,içeri doğru
@@ -541,7 +494,6 @@ const styles = StyleSheet.create({
     top: 336,
     left: 34,
     gap: 10,
-    //  borderTopWidth: 1,   // Üst kenara 1 px kalınlığında sınır
     borderRadius: 16,
     paddingTop: 6,   // üst kenar boşluğu,içeri doğru
     paddingBottom: 6,// alt kenar boşluğu,içeri doğru
@@ -558,7 +510,6 @@ const styles = StyleSheet.create({
     top: 336,
     left: 38,
     gap: 10,
-    //  borderTopWidth: 1,   // Üst kenara 1 px kalınlığında sınır
     borderRadius: 16,
     paddingTop: 6,   // üst kenar boşluğu,içeri doğru
     paddingBottom: 6,// alt kenar boşluğu,içeri doğru
@@ -575,15 +526,12 @@ const styles = StyleSheet.create({
     top: 300,
     left: 44,
     gap: 10,
-    //  borderTopWidth: 1,   // Üst kenara 1 px kalınlığında sınır
     borderRadius: 16,
     paddingTop: 6,   // üst kenar boşluğu,içeri doğru
     paddingBottom: 6,// alt kenar boşluğu,içeri doğru
     paddingLeft: 4,  // sol kenar boşluğu,içeri doğru
     paddingRight: 4, //sağ kenar boşluğu,içeri doğru
     margin: 5,
-
-
   },
 
 });
